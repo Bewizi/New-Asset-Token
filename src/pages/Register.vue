db5cf20fa7cdae368a289a184a99314384377b11
Bewizi <toluwalase.bewaji@yahoo.com>
2025-04-23 17:54:46 +0100

    worked on the lagin and signup page

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,11 +1,7 @@
-<script setup lang="ts">
-
-</script>
+<script lang="ts" setup></script>
 
 <template>
-  $END$
+  <h1>This is the register page</h1>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
